8acd48b3be144105412afe936e58539e93acca3a
David Oliveira <david.willian@aluno.unincor.edu.br>
2022-05-06 00:06:26 -0300

    fixing index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ async function startApolloServer() {
     server.applyMiddleware({ app, path: '/graphql' });
 
     await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    console.log(`🚀 Server ready at https://superheroesapibeta.herokuapp.com/${server.graphqlPath}`);
 }
 
 startApolloServer();
