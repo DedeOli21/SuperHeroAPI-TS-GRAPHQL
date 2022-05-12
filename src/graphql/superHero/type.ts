b848e4910323af9125f599d0709a797e96fb4873
David <dedepco@gmail.com>
2022-05-11 23:49:43 -0300

    Create Mutation and create type Input for fields

--- src/graphql/superHero/type.ts
+++ src/graphql/superHero/type.ts
@@ -1,6 +1,15 @@
 import { gql } from "apollo-server"
 
 export default gql`
+    input PowerstatsInput {
+        intelligence: Int
+        strength: Int
+        speed: Int
+        durability: Int
+        power: Int
+        combat: Int
+    }
+
     type Powerstats {
         intelligence: Int!
         strength: Int!
@@ -10,6 +19,15 @@ export default gql`
         combat: Int!
     }
 
+    input AppearanceInput {
+        gender: String
+        race: String
+        height: [String]
+        weight: [String]
+        eyeColor: String
+        hairColor: String
+    }
+
     type Appearance {
         gender: String!
         race: String!
@@ -17,6 +35,16 @@ export default gql`
         weight: [String]!
         eyeColor: String!
         hairColor: String!
+    }
+
+    input BiographyInput {
+        fullName: String
+        alterEgos: String
+        aliases: [String]
+        placeOfBirth: String
+        firstAppearance: String
+        publisher: String
+        alignment: String
     }
 
     type Biography {
@@ -29,14 +57,32 @@ export default gql`
         alignment: String!
     }
 
+    input WorkInput {
+        occupation: String
+        base: String
+    }
+
+
     type Work {
         occupation: String!
         base: String!
     }
 
+    input ConnectionsInput {
+        groupAffiliation: String
+        relatives: String
+    }
+
     type Connections {
         groupAffiliation: String!
         relatives: String!
+    }
+    
+    input ImagesInput {
+        xs: String
+        sm: String
+        md: String
+        lg: String
     }
 
     type Images {
@@ -60,6 +106,12 @@ export default gql`
 
     type Query {
         listHeroes(limit: Int, order: String): [SuperHero]
-        searchHeroes(filter: String, query: String): [SuperHero]
+        searchHeroes(filter: String, query: String): SuperHero
+    }
+
+    type Mutation {
+        createHero(name: String!, slug: String!, powerstats: PowerstatsInput, biography: BiographyInput, work: WorkInput, connections: ConnectionsInput, images: ImagesInput): SuperHero
+        updateHero(id: Int!, name: String!, slug: String!, powerstats: PowerstatsInput, biography: BiographyInput, work: WorkInput, connections: ConnectionsInput, images: ImagesInput): SuperHero
+        deleteHero(id: Int!): SuperHero
     }
 `
